56b0f9b4871f932cc2cdd1078d11d3e810127c57
Josh Kay <josh.m.kay@gmail.com>
2019-04-08 22:28:37 -0400

    Added landing page with an image, header and feature listings.  Navbar added with site name and non-functioning sign in and sign up buttons.

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import blue from '@material-ui/core/colors/blue';
+import pink from '@material-ui/core/colors/pink';
+
+import AppBar from './navigation/AppBar';
+import LandingPage from '../pages/LandingPage';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757de8',
+      main: '#3f51b5',
+      dark: '#002984',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ffff55',
+      main: '#ffe400',
+      dark: '#c7b200',
+      contrastText: '#000',
+    },
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
 
 class App extends Component 
 {
   render() 
   {
     return (
-      <div data-cy="app">
-        Co-Op Shop
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <AppBar />
+          <Switch>
+            <Route exact path='/' component={LandingPage} />
+          </Switch>
+        </BrowserRouter>
+      </MuiThemeProvider>
     );
   }
 }
--- cypress/integration/pages/landing.spec.ts
+++ cypress/integration/pages/landing.spec.ts
@@ -7,8 +7,23 @@ describe('Page - Landing', () =>
     cy.visit('/');
   });
 
-  it('contains Co-Op Shop', () =>
+  it('contains a heading and a background image', () =>
   {
-    cy.get('[data-cy=app]').should('have.text', 'Co-Op Shop');
+    cy.get('[data-cy=landing-header]').should('exist');
+    cy.get('[data-cy=landing-container]')
+      .should('have.css', 'background-image');
+  });
+  
+  it('contains feature listings', () =>
+  {
+    cy.get('[data-cy=landing-features]').children()
+      .should('have.length', 3)
+      .should('contain', 'Never Forget')
+      .should('contain', 'Save Time')
+      .should('contain', 'Real-time Updates')
+      .each((feature) => {
+        cy.wrap(feature).get('svg').should('exist');
+        cy.wrap(feature).get('h2').should('exist');
+      });
   });
 });
