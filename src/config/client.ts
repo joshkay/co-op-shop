98f0dfa243b693c37f7e4871951d3cced504aa74
Josh Kay <josh.m.kay@gmail.com>
2019-04-13 04:12:49 -0400

    Updated client build path served by express.  Updated dependencies.

--- src/config/client.ts
+++ src/config/client.ts
@@ -2,9 +2,9 @@ import * as path from 'path';
 
 export const init = (app, express) =>
 {
-  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.use(express.static(path.join(__dirname, '../../../client/build')));
   app.get('*', (req, res) =>
   {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../../../client/build/index.html'));
   });
 }
